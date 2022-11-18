27e84c491164cee764be27c07ae2d983eee531f8
diyorbektj <diyorbek5300@gmail.com>
2022-11-18 11:32:27 +0500

    test

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,26 +11,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
-
-    public function success(mixed $data, string $message="ok",int $statusCode=200): \Illuminate\Http\JsonResponse
-    {
-        return response()->json(
-            [
-                'data' => $data,
-                'success' => true,
-                'message' => $message
-            ], $statusCode
-        );
-    }
-
-    public function error(string $message, int $statusCode=422):\Illuminate\Http\JsonResponse
-    {
-        return response()->json(
-            [
-                'data' => null,
-                'success' => false,
-                'message'=> $message,
-            ], $statusCode
-        );
-    }
 }
